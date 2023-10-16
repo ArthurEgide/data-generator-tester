33552e73d884aa32b253f37f374cbd631ff24938
Arthur Egide <arthur@tokenology.com>
2023-10-15 21:59:20 -0300

    Add api build - Add volume on Database

--- full_pipeline.sh
+++ full_pipeline.sh
@@ -2,8 +2,10 @@
 docker network create -d bridge etl_network
 
 # Debug loader API
+docker build -t tcc_loader tcc_loader
 docker run --network etl_network -it --rm tcc_loader
 
 # Set up Faker-JS project
 docker build -t generator_fakerjs generators/faker-js_faker/
-docker run --network etl_network -it --rm generator_fakerjs run
+docker run --network etl_network -it --rm -v ./generators/faker-js_faker/:/generator generator_fakerjs
+#  run
