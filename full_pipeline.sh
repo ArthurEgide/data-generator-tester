3d55763240a5fa9c687a1da858bf3220de2e6a3f
Arthur Egide <arthur@tokenology.com>
2023-10-22 22:18:57 -0300

    change network name

--- full_pipeline.sh
+++ full_pipeline.sh
@@ -1,10 +1,14 @@
 # Create common bridge network
-docker network create -d bridge etl_network
+docker network create tcc_egide
 
 # Debug loader API
 docker build -t tcc_loader tcc_loader
-docker run --network etl_network -it --rm tcc_loader
+docker run --network tcc_egide -it --rm tcc_loader
 
 # Set up Faker-JS project
 docker build -t generator_fakerjs generators/faker-js_faker/
-docker run --network etl_network --memory="1g" --cpus="2.0" -it --rm -v ./generators/faker-js_faker/:/generator generator_fakerjs run
+docker run --network tcc_egide --memory="1g" --cpus="2.0" -it --rm -v ./generators/faker-js_faker/:/generator generator_fakerjs run
+
+# Set up Fakedata
+docker build -t generator_fakedata generators/lucapette-fakedata/
+docker run --network tcc_egide --memory="1g" --cpus="2.0" -it --rm -e PGPASSWORD=egide -v ./generators/lucapette-fakedata/:/generator generator_fakedata run
