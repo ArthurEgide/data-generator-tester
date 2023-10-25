9f50d21b6aa7c6e4be0b54b53cafab0711645ce3
Arthur Egide <arthur@tokenology.com>
2023-10-25 02:10:59 -0300

    Add jfairy

--- full_pipeline.sh
+++ full_pipeline.sh
@@ -7,8 +7,12 @@ docker run --network tcc_egide -it --rm tcc_loader
 
 # Set up Faker-JS project
 docker build -t generator_fakerjs generators/faker-js_faker/
-docker run --network tcc_egide --memory="1g" --cpus="2.0" -it --rm -v ./generators/faker-js_faker/:/generator generator_fakerjs run
+docker run --network tcc_egide --memory="1g" --cpus="2.0" -it --rm -v ./generators/faker-js_faker/:/usr/generator generator_fakerjs run
 
 # Set up Fakedata
 docker build -t generator_fakedata generators/lucapette-fakedata/
-docker run --network tcc_egide --memory="1g" --cpus="2.0" -it --rm -e PGPASSWORD=egide -v ./generators/lucapette-fakedata/:/generator generator_fakedata run
+docker run --network tcc_egide --memory="1g" --cpus="2.0" -it --rm -e PGPASSWORD=egide -v ./generators/lucapette-fakedata/:/usr/generator generator_fakedata run
+
+# Set up Fakedata
+docker build -t generator_jfairy generators/devskiller_jfairy/
+docker run --network tcc_egide --memory="1g" --cpus="2.0" -it --rm -v ./generators/devskiller_jfairy/:/usr/generator generator_jfairy
